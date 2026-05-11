def8aff3e49ad20022cd7a2bbb35f28636bf7989
j-colours <shrewsburyj2@winthrop.edu>
2026-05-11 00:19:17 -0400

    Mana #6: added previews for both and started moving all resources in directory somewhere else

--- mana.sh
+++ mana.sh
@@ -5,16 +5,22 @@
 # Date: 2026-05-10
 # Description: Resource Manager to streamline movement of files and the creation of them.
 
+# declare ANSI Escape Codes for colors
+RESET='\x1b[0m'
+RED='\x1b[31m'
+
 # Function Name: single_file
 # Purpose: Whenever -s or --single flag is called run management for specfic file
 # mana <file name> <new_directory>
 single_file() {
   FILE=$1
   TO_DIR=$2
+  PREVIEW=$3
 
   # error checking for file and directory
   if [[ ! -f "$FILE" ]]; then
-    echo $'\x1b[31m'"Error: File doesn't exist... Please make sure it is the correct name..."$'\x1b[0m'
+    echo
+    echo -e "${RED}Error: File doesn't exist... Please make sure it is the correct name...${RESET}"
     exit 1 # failure status
   fi
 
@@ -22,13 +28,26 @@ single_file() {
     mkdir -p "$TO_DIR"
   fi
 
-  echo "$FILE --> $TO_DIR"
-  mv "$FILE" "$TO_DIR"
+  if [[ "$PREVIEW" == true ]]; then
+    echo
+    echo "$FILE --> $TO_DIR"
+  else
+    echo
+    echo "$FILE --> $TO_DIR"
+    mv "$FILE" "$TO_DIR"
+  fi
 }
 
 # Function Name: preview_single
 # Purpose: Whenever -ps or --preview-single show what would happen if single_file is used
 # mana (-ps|--preview-single) <file name> <new_directory>
+preview_single() {
+  FILE=$1
+  TO_DIR=$2
+  PREVIEW=true
+
+  single_file "$FILE" "$TO_DIR" "$PREVIEW"
+}
 
 # Function Name: mult_files
 # Purpose: Whenever -m or --mult flag is called run management for multiple files
@@ -38,16 +57,30 @@ mult_files() {
   DIR=$1
   TYPE=$2
   TO_DIR=$3
+  PREVIEW=$4
+
+  # check if type exists --> sees if find returns a string of nothing
+  if [[ -z "$(find "$DIR" -iname "*.$TYPE")" ]]; then
+    echo
+    echo -e "${RED}Error: Type doesn't exist... Please make sure it is the correct name...${RESET}"
+    exit 1 # failure status
+  fi
 
   # check if directory exists because if not then create it
   if [[ ! -d "$TO_DIR" ]]; then
     mkdir -p "$TO_DIR"
   fi
 
+  echo
+
   # read each file recursively found and move it to destination
   find "$DIR" -iname "*.$TYPE" | while read -r FILE; do
-    echo "$FILE --> $TO_DIR"
-    mv "$FILE" "$TO_DIR"
+    if [[ "$PREVIEW" == true ]]; then
+      echo "$FILE --> $TO_DIR"
+    else
+      echo "$FILE --> $TO_DIR"
+      mv "$FILE" "$TO_DIR"
+    fi
   done
 }
 
@@ -56,7 +89,18 @@ mult_files() {
 # without actually doing it
 # mana (-pm|--preview-mult) <directory> <file type/extension> <new_directory>
 preview_mult() {
-  echo "Placeholder here..."
+  DIR=$1
+  TYPE=$2
+  TO_DIR=$3
+  PREVIEW=true
+
+  mult_files "$DIR" "$TYPE" "$TO_DIR" "$PREVIEW"
+}
+
+# Function Name: move_all
+# Purpose: -ma or --move-all moves everything in a directory into somewhere else
+move_all() {
+  echo "Placeholer here..."
 }
 
 # Function Name: main
@@ -67,13 +111,19 @@ main() {
 
   case "$FLAG" in
   "-m" | "--mult")
-    mult_files "$2" "$3" "$4"
+    mult_files "$2" "$3" "$4" "$PREVIEW"
     ;;
   "-s" | "--single")
-    single_file "$2" "$3"
+    single_file "$2" "$3" "$PREVIEW"
+    ;;
+  "-ps" | "--preview-single")
+    preview_single "$2" "$3"
+    ;;
+  "-pm" | "--preview-mult")
+    preview_mult "$2" "$3" "$4"
     ;;
   *)
-    echo $'\x1b[31m'"Error: $FLAG is not a valid flag..."'$\x1b[0m'
+    echo "${RED}Error: $FLAG is not a valid flag...${RESET}"
     ;;
   esac
 
